fde0ce7b13924291b77ab502d7dd51f42ff1c978
Giancarlo Gamarra <youthlinksdev@sos-kd.org>
2018-11-30 07:12:24 -0500

    First Commit

---
+++ Amazon/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.Models;
+
+namespace Amazon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Goog Morning" : "Good Afternoon";
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult RegBookForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RegBookForm(BookResponse bookResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(bookResponse);
+                return View("Thanks", bookResponse);
+            }
+            else {
+                // Hay un error de validacion y retornamos una vista en blanco. 
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ViewResult ListResponses()
+        {
+            //return View(Repository.Responses.Where(b => b.Price > 100)); Libros caros
+            //return View(Repository.Responses.Where(b => b.Price < 100)); Libros baratos
+            return View(Repository.Responses);
+        }
+    }
+}
+
+
---
+++ Amazon/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+namespace Amazon.Models
+{
+    public class Repository
+    {
+        public static List<BookResponse> responses = new List<BookResponse>();
+        public static IEnumerable<BookResponse> Responses {
+            get {
+                return responses;
+                }
+        }
+        public static void AddResponse(BookResponse response) {
+            responses.Add(response);
+        }
+    }
+}
