e987e5438deafa182de377c8f9f27edbec7eff2b
Giancarlo Gamarra <youthlinksdev@sos-kd.org>
2018-12-04 23:05:55 -0500

    Contenido actualizado con boostrap y views con layout

--- Amazon/Models/Repository.cs
+++ Amazon/Models/Repository.cs
@@ -11,36 +11,39 @@ namespace Amazon.Models
 
         public Repository()
         {
-            BookResponse book1 = new BookResponse()
+            if (books.Count == 0)
             {
-                ISBN = "123456789",
-                Author = "Giancarlo G",
-                NroPages = 210,
-                Price = 270,
-                Title = "How to Program ASP.NET MVC"
-            };
-            BookResponse book2 = new BookResponse()
-            {
-                ISBN = "521648597",
-                Author = "Giancarlo G",
-                NroPages = 3200,
-                Price = 3000,
-                Title = "How to Program C#"
-            };
-            BookResponse book3 = new BookResponse()
-            {
-                ISBN = "258456",
-                Author = "Giancarlo G",
-                NroPages = 500,
-                Price = 200,
-                Title = "How to Program Java",
-                LevelStock = LevelStock.SoldOut
-            };
+                BookResponse book1 = new BookResponse()
+                {
+                    ISBN = "123456789",
+                    Author = "Giancarlo G",
+                    NroPages = 210,
+                    Price = 270,
+                    Title = "How to Program ASP.NET MVC"
+                };
+                BookResponse book2 = new BookResponse()
+                {
+                    ISBN = "521648597",
+                    Author = "Giancarlo G",
+                    NroPages = 3200,
+                    Price = 3000,
+                    Title = "How to Program C#"
+                };
+                BookResponse book3 = new BookResponse()
+                {
+                    ISBN = "258456",
+                    Author = "Giancarlo G",
+                    NroPages = 500,
+                    Price = 200,
+                    Title = "How to Program Java",
+                    LevelStock = LevelStock.SoldOut
+                };
 
-            books.Add(book1);
-            books.Add(book2);
-            books.Add(book3);
-            books.Add(null);
+                books.Add(book1);
+                books.Add(book2);
+                books.Add(book3);
+                books.Add(null);
+            }
         }
 
         public IEnumerable<BookResponse> Books => books;
